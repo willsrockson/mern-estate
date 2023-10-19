084628dccac19989138472b156424ff34b655f90
Wills Rockson <marleyenos18@gmail.com>
2023-10-19 22:07:04 +0000

    Connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+
+//connected to Database through dot env
+mongoose
+    .connect(process.env.MONGO)
+    .then((res) => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 // The port of the backend
 app.listen(3000, () => {
