af6a4f8c45e3bd4e0d2493398d20a79d6fb254ee
Wills Rockson <marleyenos18@gmail.com>
2023-10-20 14:46:17 +0000

    Created sign in api route/functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import { User } from "../index.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+// handles signing up to the site
 export const singup = async (req, res, next) => {
     const { email, username, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -11,5 +13,26 @@ export const singup = async (req, res, next) => {
         res.status(201).json("user created successfuly");
     } catch (error) {
         next(error); //Displays the error handler in Index.js
+    }
+};
+
+//this handles signing in to the page
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(
+            password,
+            validUser.password
+        );
+        if (!validPassword) return next(errorHandler(404, "Wrong credentials"));
+        const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET); //Token for the cookies
+        const { password: pass, ...rest } = validUser._doc; // return all the things except the password
+        res.cookie("access_token", token, { httOnly: true })
+            .status(200)
+            .json(rest); //this creates a cookie in the browser
+    } catch (error) {
+        next(error);
     }
 };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { singup } from "../controllers/auth.controller.js";
+import { signin, singup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", singup);
+router.post("/signin", signin);
 
 export default router;
