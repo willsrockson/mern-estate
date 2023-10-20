6a22267904c44e2a308b55924517f6ef64cc4bf7
Wills Rockson <marleyenos18@gmail.com>
2023-10-20 11:42:48 +0000

    Complete signup page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,17 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        try {
+            setLoading(true);
+            event.preventDefault();
+            // to send a form to a server stringify it
+            const res = await fetch("/api/auth/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData), //this is saying send the data from the body in a string format
+            });
+
+            const data = await res.json();
+            if (data.success == false) {
+                setLoading(false);
+                setError(data.message);
+
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate("/sign-in");
+        } catch (error) {
+            setLoading(false);
+            setError(error.message);
+        }
+    };
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Sign up</h1>
 
-            <form className="flex flex-col gap-4">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                 <input
                     type="text"
                     placeholder="username"
                     className="border p-3 rounded-lg"
                     id="username"
+                    onChange={handleChange}
                 />
 
                 <input
@@ -19,6 +62,7 @@ export default function Signup() {
                     placeholder="email"
                     className="border p-3 rounded-lg"
                     id="email"
+                    onChange={handleChange}
                 />
 
                 <input
@@ -26,9 +70,14 @@ export default function Signup() {
                     placeholder="password"
                     className="border p-3 rounded-lg"
                     id="password"
+                    onChange={handleChange}
                 />
-                <button className="bg-slate-700 p-3 text-white rounded-lg hover:bg-slate-600 disabled:opacity-80">
-                    Sign up
+                <button
+                    type="submit"
+                    disabled={loading}
+                    className="bg-slate-700 p-3 text-white rounded-lg uppercase hover:bg-slate-600 disabled:opacity-80"
+                >
+                    {loading ? "Loading..." : "Sign up"}
                 </button>
             </form>
             <div className="flex gap-2 mt-5">
@@ -37,6 +86,7 @@ export default function Signup() {
                     <span className="text-blue-700">sign in</span>
                 </Link>
             </div>
+            {error && <p className="text-red-500 mt-5">{error}</p>}
         </div>
     );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000", // anytime you see /api add localhost 3000 in front
+                secure: false,
+            },
+        },
+    },
+    plugins: [react()],
+});
